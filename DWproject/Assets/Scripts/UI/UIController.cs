2f49a4ca3b407be656ca941c30267825d3550f69
Helena-code <helenakovaleva.gamedev@gmail.com>
2026-07-15 19:53:37 +0400

    added UI controller

--- DWproject/Assets/Scripts/UI/UIController.cs
+++ DWproject/Assets/Scripts/UI/UIController.cs
@@ -1,9 +1,42 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class UIController: MonoBehaviour
 {
-    [SerializeField] private Button leftArrow;
-    [SerializeField] private Button rightArrow;
-
+    [SerializeField] private Button leftLocationButton;
+    [SerializeField] private Button rightLocationButton;
+    
+    private Func<Location> getLocation;
+    
+    public void Init(Action<string> onLocationButtonClicked, GameState state)
+    {
+        leftLocationButton.onClick.AddListener(()=> onLocationButtonClicked?.Invoke(Consts.LeftButton));
+        rightLocationButton.onClick.AddListener(()=> onLocationButtonClicked?.Invoke(Consts.RightButton));
+        Refresh(state);
+    }
+    
+    public void Refresh(GameState state)
+    {
+        switch (state.CurrentLocation)
+        {
+            case Location.Location1:
+                leftLocationButton.gameObject.SetActive(state.HasCheckedSafe);
+                rightLocationButton.gameObject.SetActive(state.HasTalkedToNpc);
+                break;
+            case Location.Location2:
+                leftLocationButton.gameObject.SetActive(true);
+                rightLocationButton.gameObject.SetActive(false);
+                break;
+            case Location.Location3:
+                leftLocationButton.gameObject.SetActive(false);
+                rightLocationButton.gameObject.SetActive(true);
+                break;
+            case Location.Main:
+            default:
+                leftLocationButton.gameObject.SetActive(false);
+                rightLocationButton.gameObject.SetActive(false);
+                break;
+        }
+    }
 }
