bc5decdf7a5ddcb45003532bc9b9f373726df89c
Helena-code <helenakovaleva.gamedev@gmail.com>
2026-07-16 09:47:05 +0400

    added inventory item UI

--- DWproject/Assets/Scripts/Core/DisplayController.cs
+++ DWproject/Assets/Scripts/Core/DisplayController.cs
@@ -3,14 +3,38 @@ using UnityEngine;
 public class DisplayController : MonoBehaviour
 {
     [SerializeField] private GameObject npc;
+    [SerializeField] private GameObject key;
+    [SerializeField] private GameObject safe;
 
+    private bool isLocationAllowsVisibleSafe = false;
+    private bool isLocationAllowsVisibleKey = false;
+    private bool isHiddenKey = false;
+    private bool isLocationAllowsVisibleNPC = false;
+    private bool isHiddenByDialogueNPC = false;
+
+    // for commands usage
     public void ChangeVisibility(bool isVisible)
     {
-        if (npc != null) npc.SetActive(isVisible);
+        isHiddenByDialogueNPC = !isVisible;
+        Apply();
     }
 
+    // for c#
     public void Refresh(GameState state)
     {
-        //ChangeVisibility(state.CurrentLocation == Location.Location1);
+        isLocationAllowsVisibleNPC = state.CurrentLocation == Location.Location1;
+        isLocationAllowsVisibleSafe = state.CurrentLocation == Location.Location2;
+        isLocationAllowsVisibleKey = state.CurrentLocation == Location.Location3;
+        Apply();
+    }
+
+    private void Apply()
+    {
+        if (npc != null)
+        {
+            npc.SetActive(isLocationAllowsVisibleNPC && !isHiddenByDialogueNPC);
+            safe.SetActive(isLocationAllowsVisibleSafe);
+            key.SetActive(isLocationAllowsVisibleKey);
+        }
     }
 }
