b8a0c17da5ddd9826db472661175170138976b2a
Helena-code <helenakovaleva.gamedev@gmail.com>
2026-07-17 11:43:29 +0400

    setup inventory icon

--- DWproject/Assets/Scripts/Core/DisplayController.cs
+++ DWproject/Assets/Scripts/Core/DisplayController.cs
@@ -33,8 +33,8 @@ public class DisplayController : MonoBehaviour
         if (npc != null)
         {
             npc.SetActive(isLocationAllowsVisibleNPC && !isHiddenByDialogueNPC);
-            safe.SetActive(isLocationAllowsVisibleSafe);
-            key.SetActive(isLocationAllowsVisibleKey);
         }
+        safe.SetActive(isLocationAllowsVisibleSafe);
+        key.SetActive(isLocationAllowsVisibleKey);
     }
 }
