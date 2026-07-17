22c23665f0ee7a64aea9124337b06fe2159fa841
Helena-code <helenakovaleva.gamedev@gmail.com>
2026-07-17 11:42:53 +0400

    added new game states

--- DWproject/Assets/Scripts/Core/GameState.cs
+++ DWproject/Assets/Scripts/Core/GameState.cs
@@ -6,17 +6,31 @@ public class GameState
 {
     public bool HasTalkedToNpc { get; private set; }
     public bool HasCheckedSafe { get; private set; }
+    public bool HasMinigameWinned { get; private set; }
+    public bool HasSafeOpened { get; private set; }
     public Location CurrentLocation { get; private set; }
 
-    public GameState(bool hasTalkedToNpc, bool hasCheckedSafe, Location currentLocation)
+    public GameState(
+        bool hasTalkedToNpc, 
+        bool hasCheckedSafe,
+        Location currentLocation,
+        bool hasMinigameWinned,
+        bool hasSafeOpened)
     {
-        Update(hasTalkedToNpc, hasCheckedSafe, currentLocation);
+        Update(hasTalkedToNpc, hasCheckedSafe, currentLocation, hasMinigameWinned, hasSafeOpened);
     }
 
-    public void Update(bool hasTalkedToNpc, bool hasCheckedSafe, Location currentLocation)
+    public void Update(
+        bool hasTalkedToNpc, 
+        bool hasCheckedSafe, 
+        Location currentLocation, 
+        bool hasMinigameWinned,
+        bool hasSafeOpened)
     {
         HasTalkedToNpc = hasTalkedToNpc;
         HasCheckedSafe =  hasCheckedSafe;
         CurrentLocation = currentLocation;
+        HasMinigameWinned = hasMinigameWinned;
+        HasSafeOpened = hasSafeOpened;
     }
 }
---
+++ DWproject/Assets/Scripts/Minigame/GameResult.cs
@@ -0,0 +1,7 @@
+public enum GameResult
+{
+    None = 0,
+    PlayerWon = 1,
+    PlayerLost = 2,
+    Draw = 3
+}
